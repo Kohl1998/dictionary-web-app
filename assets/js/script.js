619cd21c1cd8e54f0b6a2f698322df1bbbaccb35
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-06-17 23:29:50 +0100

    added ajax call for fetch to dictionary api

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,43 +8,58 @@ let img = $('#moonIcon')
 let inputField = $('#inputField')
 
 // To hold api response
-let results = []
-
+let result = []
 
 // Function to change image src once toggle button is clicked
 toggle.on('click', function () {
-    img.attr('src', 'assets/images/crescent-moon-svgrepo-dark.svg') 
+    img.attr('src', 'assets/images/crescent-moon-svgrepo-dark.svg')
+    $('body').css({ "background-color": "black" })
+    $('#mainContent').css({ "background-color": "black", "color": "white" })
     console.log(input)
 })
 
 
-inputField.on('keypress', function(e) {
+inputField.on('keypress', function (e) {
     if (e.which == 13) {
-        let userValue = $('#search').val()
+        var userValue = $('#search').val()
         console.log(userValue)
         let root = $('#root')
         let title = $('<h1>')
         title.text(userValue)
         /* class in css sheet to create space between user input and input box */
-        title.addClass('inputMargin')
-        root.append(title)}
-    })
-    
-    // Function to toggle between Google fonts
-    $('#font').change(function() {
-        // to show users option from dropdown
-        let value = $(this).val()
-        alert(`You have selected ${value}`)
-        $('body').css({"font-family": value})
-    })
-
-let result = $('#apiResponse')
-
-/* function searchWord() {
-    const word = $('#search').val()
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+        title.addClass('marginTop')
+        root.append(title)
+    }
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${userValue}`
     $.ajax({
-        method: "GET",
         url: url,
+        method: "GET",
+        dataType: 'json',
+        success: function (response) {
+            console.log(response[0].license)
+        },
+        error: function (error) {
+            console.log(error)
+            // plays audio if error in GET request
+            $('audio#pop')[0].play()
+        }
     })
-} */
+}
+)
+
+// Function to toggle between Google fonts
+$('#font').change(function () {
+    // to show users option from dropdown
+    let value = $(this).val()
+    alert(`You have selected ${value}`)
+    // updates font family with option selected
+    $('body').css({ "font-family": value })
+})
+
+// Empties search content
+let reset = $('#reset').on('click', function () {
+    $('#search').val('')
+    let root = $('#root')
+    root.empty()
+})
+
